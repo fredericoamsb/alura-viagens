8092dfc07502e3faa80b30a65082089c258a558f
Frederico <fred@soujmv.com>
2020-09-13 19:18:52 -0300

    Initial Commit

---
+++ alura-viagens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alura-viagens
+//
+//  Created by Frederico Augusto on 13/09/20.
+//  Copyright © 2020 Frederico Augusto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
